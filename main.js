2fe1e65b84b64fca297f784bf64696ce69ec8a73
mnmlst <145312658+AbdulrahmanSE2003@users.noreply.github.com>
2024-12-17 20:31:00 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+// <!--* Navbar => JS -->
+const navLis = document.querySelectorAll(".navbar-links .main-li");
+
+addActiveClass(navLis);
+// SECTION - Add & Remove Active Class.
+function addActiveClass(nodeList) {
+  nodeList.forEach((a) => {
+    a.addEventListener("click", (e) => {
+      nodeList.forEach((a) => {
+        a.classList.remove("active");
+      });
+      a.classList.add("active");
+    });
+  });
+}
+
+
 const text = ["Learn More About Our Faculty", "Explore Departments & Expertise", "Leaders in Education", "Shaping Future Generations"];
 let index = 0;
 let charIndex = 0;
@@ -6,34 +23,6 @@ const erasingSpeed = 95;
 const delayBetweenTexts = 2000; 
 const typedText = document.getElementById("typed-text");
 const cursor = document.querySelector(".cursor");
-
-// function type() {
-//   if (charIndex < text[index].length) {
-//     typedText.textContent += text[index].charAt(charIndex);
-//     charIndex++;
-//     setTimeout(type, typingSpeed);
-//   } else {
-//     setTimeout(erase, delayBetweenTexts);
-//   }
-// }
-
-// function erase() {
-//   if (charIndex > 0) {
-//     typedText.textContent = text[index].substring(0, charIndex - 1);
-//     charIndex--;
-//     setTimeout(erase, erasingSpeed);
-//   } else {
-//     index = (index + 1) % text.length; // Move to the next text
-//     setTimeout(type, typingSpeed);
-//   }
-// }
-
-// // Start the typing effect
-// document.addEventListener("DOMContentLoaded", () => {
-//   setTimeout(type, delayBetweenTexts);
-// });
-
-
 
 function type(){
   if (charIndex < text[index].length){
